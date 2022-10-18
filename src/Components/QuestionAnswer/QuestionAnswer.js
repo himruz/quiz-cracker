4189b239ad66a242a56c29a4567a601f1467fb2a
himruz <imruz2001@gmail.com>
2022-10-18 19:27:26 +0600

    update

--- src/Components/QuestionAnswer/QuestionAnswer.js
+++ src/Components/QuestionAnswer/QuestionAnswer.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import './QuestionAnswer.css'
+import { EyeIcon } from '@heroicons/react/24/solid'
 const QuestionAnswer = ({questionData, index} ) => {
-    const {question} = questionData;
-    console.log(question)
+    const {question, options} = questionData;
+    // console.log(options)
+    const correctAnswer = (answer) =>{
+        console.log(options[answer])
+    } 
     return (
-        <div className="row">
-            <h4>Quiz of {index+1}</h4>
+        <div>
+            <div className="row mt-5 mb-5">
+                <h4>Quiz {index+1} : {question} <span><EyeIcon style={{ width: '18px'}}></EyeIcon></span></h4>
+            </div>
+            <div className="row">
+                <div className='option-wrapper'>
+                    {
+                        options.map((option, index) => <h5 key={index} onClick={()=>{correctAnswer(index)}} className='shadow-lg'>{option}</h5>)
+                    }
+                </div>
+            </div>
         </div>
+        
     );
 };
 
